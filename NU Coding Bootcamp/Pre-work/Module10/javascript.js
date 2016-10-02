7d1370d23f54a45bd255890853fdb1ecfef417f7
Kevin Tam <tam580@gmail.com>
2016-10-02 16:34:00 -0500

    10/2 revision, index.h.tml

---
+++ NU Coding Bootcamp/Pre-work/Module10/javascript.js
@@ -0,0 +1,27 @@
+$("#button1").on("click", function(){
+
+    $("#box").css("height", "250px");
+    $("#box").css("width", "250px");
+
+
+});
+
+$("#button2").on("click", function(){
+
+    $("#box").css("background-color", "blue");
+
+});
+
+$("#button3").on("click", function(){
+
+    $("#box").css("opacity", "0.5");
+
+});
+
+$("#button4").on("click", function(){
+
+    $("#box").css("background-color", "orange");
+    $("#box").css("opacity", "1.0");
+    $("#box").css("height", "150px");
+
+});
